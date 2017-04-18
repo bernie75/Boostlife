7aa38b68aabb9a2beb1009449fe1a0f31511006f
Yas la menace <yasbernays@gmail.com>
2017-04-18 16:53:16 +0200

    fixing js on heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @articles = Article.last(3)
+    @articles = Article.all.sample(3)
   end
 
 end
